346cca86f9cd9fc8fda22a1e7431d7491b015f42
Akame0xg1rl <ikrambouachra2@gmail.com>
2024-06-05 21:42:48 +0100

    binary_size

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,6 +2,7 @@
 
 /**
  * binary_tree_depth - Measures the depth of a node in a binary tree
+ *
  * @tree: Pointer to the node to measure the depth
  *
  * Return: Depth of the node. If tree is NULL, return 0.
@@ -10,6 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
+<<<<<<< HEAD
 	if (tree == NULL)
 		return (0);
 	while (tree->parent != NULL)
@@ -18,4 +20,16 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		tree = tree->parent;
 	}
 	return (depth);
+=======
+    if (tree == NULL)
+        return (0);
+
+    while (tree->parent != NULL)
+    {
+        depth++;
+        tree = tree->parent;
+    }
+
+    return (depth);
+>>>>>>> 558ba5b (binary_size)
 }
