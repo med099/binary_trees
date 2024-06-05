f7a6e62f615d72d3b7b6751f9d243a8466c1db7c
med099 <138779714+med099@users.noreply.github.com>
2024-06-05 18:02:19 +0000

    fix betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t	left_height;
+	size_t	right_height;
 
-    if (tree == NULL)
-        return 0;
-
-    if (tree->left == NULL && tree->right == NULL)
-        return 0;
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (left_height > right_height ? left_height : right_height) + 1;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	printf("left = %lu\n", left_height);
+	printf("right = %lu\n", right_height);
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
